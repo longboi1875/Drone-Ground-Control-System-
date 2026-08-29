ce300f84a02f5ee7556e354fbc8e1fa9789cfbcb
gurnek <gurnek@mercedesandsingh.com>
2026-08-28 17:02:00 -0700

    Simplify the PX4 setup a little

--- scripts/setup_px4.sh
+++ scripts/setup_px4.sh
@@ -7,11 +7,15 @@ PX4_VERSION="${SKYLINK_PX4_VERSION:-v1.17.0}"
 
 mkdir -p "$(dirname "$PX4_DIR")"
 if [[ ! -d "$PX4_DIR/.git" ]]; then
-  git clone --branch "$PX4_VERSION" --depth 1 --recurse-submodules --shallow-submodules \
+  git clone --branch "$PX4_VERSION" --depth 1 \
     https://github.com/PX4/PX4-Autopilot.git "$PX4_DIR"
 fi
 
 cd "$PX4_DIR"
+git submodule update --init --depth 1 \
+  Tools/simulation/gz \
+  src/lib/events/libevents \
+  src/modules/mavlink/mavlink
 ./Tools/setup/macos.sh --sim-tools
 make px4_sitl_default
 
